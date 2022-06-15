bc6995383bbcab15ab11d3c7596404eb4046600f
Miriam Anyango <minaoanya@gmail.com>
2022-06-15 10:07:30 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+// function superbowlWin(record){
+//   let result =record.find((record)=>record.result==="W")
+// return  result ? record.year : undefined;
+// }
+function superbowlWin(record){
+  let result =record.find(record=>
+    record.result==="W")
+  
+  return  result ? result.year : undefined;
+}
